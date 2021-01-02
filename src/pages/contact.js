b12f4c1ac7732e0a3bce689388bbf7524a197f45
schwabthedeck <jane.a.schwab@gmail.com>
2021-01-02 16:08:14 -0500

    Contact form work

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,15 +11,15 @@ const ContactPage = () => (
     <Form name="contact" method="post" action="/thanks/" data-netlify="true" data-netlify-honeypot="bot-field">
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" name="email" />
       </Form.Group>
       <Form.Group controlId="formBasicSubject">
         <Form.Label>Subject</Form.Label>
-        <Form.Control type="text" placeholder="Subject" />
+        <Form.Control type="text" placeholder="Subject" name="subject" />
       </Form.Group>
       <Form.Group controlId="formBasicMessage">
         <Form.Label>Message</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Control as="textarea" rows={3} name="message" />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
